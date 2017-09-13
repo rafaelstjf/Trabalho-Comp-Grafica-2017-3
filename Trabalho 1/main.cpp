0f5e9a54fc659ad7f005c7c16492921d6703b9b3
rafaelstjf <rafaelstjf@hotmail.com>
2017-09-13 16:31:04 -0300

    Placar
    implementacoes do placar

--- Trabalho 1/main.cpp
+++ Trabalho 1/main.cpp
@@ -10,6 +10,7 @@
 #include "filaAnima.h"
 #include "explosao.h"
 #include "menu.h"
+#include "Placar.h"
 #ifdef __linux__
 #include <cstring>
 #endif
@@ -29,11 +30,16 @@ int width = 800;
 int height = 450;
 float jogadorx = 0, jogadory = 0, mousex, mousey; //posicao do jogador e do mouse
 int pontos = 0;                                   //variavel para contar os pontos
-int vidas = 10;                                   //variavel para controle de vidas
+int vidas = 10;                            //variavel para controle de vidas
+int indPlacar=0;
 int dificuldade = 1;                              //variavel para controle de dificuldade
 bool mouseDown = false;
 bool fullscreen = false;
 float clok = 0;
+bool emPlacar = false, confirmaInsercao=false;
+Placar *pl = new Placar();
+char nome[20];
+
 //funcoes
 void idle();
 void display();
@@ -45,6 +51,8 @@ void reshape(int w, int h);
 void keyboardPress(unsigned char key, int x, int y);
 void specialKeysPress(int key, int x, int y);
 void startWindow(int argc, char **argv);
+void exibePlacar();
+
 menu inicio;
 bool comecou = false;
 bool opcao = 0;
@@ -54,6 +62,11 @@ filaAnima inimigos = filaAnima(10);
 int main(int argc, char **argv)
 {
     srand(time(NULL));
+    nome[0] = 'A';
+    for(int i=1; i< 20; i++)
+    {
+        nome[i] = ' ';
+    }
     startWindow(argc, argv);
     glutMainLoop();
     return 0;
@@ -75,7 +88,54 @@ void startWindow(int argc, char **argv)
     glutDisplayFunc(display);
     glutIdleFunc(idle);
 }
+void exibePlacar() //funcao para printar o placar na tela
+{
+    glColor3f(0, 0, 0);     //cor do texto
 
+    int len;
+    string output, titulo ="PLACAR";
+    glRasterPos2f(50, 80);
+    for(int i=0; i<6; i++)
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, titulo[i]); //printa com a fonte GLUT_BITMAP_8_BY_13
+    for(int k = 0; k<10; k++)
+    {
+
+        glColor3f((k*1.0)/10,0, 1-((k*1.0)/10));
+        output = pl->getPosicao(k);
+        len = (int)strlen(output.c_str());
+        glRasterPos2f(45, 50-10*k);
+        for (int i = 0; i < len; i++)
+        {
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, output[i]);
+        }
+    }
+    if(!confirmaInsercao)
+    {
+        /*  //desenha triangulos
+          glColor3f(0.0, 0.0, 1.0);
+          glBegin(GL_TRIANGLES);
+          glVertex2f(46+4.5*indPlacar, -53);
+          glVertex2f(47 +4.5*indPlacar, -51);
+          glVertex2f(48+4.5*indPlacar, -53);
+          glEnd();
+          glBegin(GL_TRIANGLES);
+          glVertex2f(46+4.5*indPlacar, -63);
+          glVertex2f(47 +4.5*indPlacar, -65);
+          glVertex2f(48+4.5*indPlacar, -63);
+          glEnd();
+        */
+        for (int i = 0; i < 20; i++)
+        {
+            //local
+            if(i==indPlacar)
+                glColor3f(1.0, 0, 0);     //cor do texto
+            else
+                glColor3f(0, 0, 0);     //cor do texto
+            glRasterPos2f(45+5.5*i, -60);
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, nome[i]);
+        }
+    }
+}
 void drawAim()
 {
     glColor3f(0.0, 0.0, 1.0);
@@ -99,108 +159,158 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    if(!comecou){
-    inicio.draw((int)opcao);
-    }else{
-    explosoes.desenhos();
-    inimigos.desenhos();
-    //teste->draw(jogadorx, -jogadory,1.0,0.0,0.0);
-    /******   CANHOES  *******/
-    glColor3f(0, 0, 1);
-    glBegin(GL_QUADS);
-    glVertex2f(110, -88.75);
-    glVertex2f(90, -88.75);
-    glVertex2f(90, -77.5);
-    glVertex2f(110, -77.5);
-    glEnd();
-    glBegin(GL_QUADS);
-    glVertex2f(-70, -88.75);
-    glVertex2f(-90, -88.75);
-    glVertex2f(-90, -77.5);
-    glVertex2f(-70, -77.5);
-    glEnd();
-    glBegin(GL_QUADS);
-    glVertex2f(290, -88.75);
-    glVertex2f(270, -88.75);
-    glVertex2f(270, -77.5);
-    glVertex2f(290, -77.5);
-    glEnd();
-    /***** FIM CANHOES *****/
-    /*****   CIDADES   *****/
-    glColor3f(0, 1, 0);
-    for (int cd = -60; cd <= 240; cd += 50)
+    if(!comecou&& !emPlacar)
     {
-        if (cd != 90)
+
+        inicio.draw((int)opcao);
+    }
+    else if(!comecou && emPlacar)
+    {
+        //borda Externa
+        glColor3f(0.0, 0.0, 1.0);
+        glBegin(GL_LINE_LOOP);
+        glVertex2f(-60,-90);
+        glVertex2f(260, -90);
+        glVertex2f(260, 115);
+        glVertex2f(-60, 115);
+        glEnd();
+        //Borda Interna
+        glColor3f(0.0,0.0,1.0);
+        glBegin(GL_LINE_LOOP);
+        glVertex2f(-59,-89);
+        glVertex2f(259, -89);
+        glVertex2f(259, 114);
+        glVertex2f(-59, 114);
+        glEnd();
+        exibePlacar();
+
+    }
+
+    else
+    {
+        explosoes.desenhos();
+        inimigos.desenhos();
+        //teste->draw(jogadorx, -jogadory,1.0,0.0,0.0);
+        /******   CANHOES  *******/
+        glColor3f(0, 0, 1);
+        glBegin(GL_QUADS);
+        glVertex2f(110, -88.75);
+        glVertex2f(90, -88.75);
+        glVertex2f(90, -77.5);
+        glVertex2f(110, -77.5);
+        glEnd();
+        glBegin(GL_QUADS);
+        glVertex2f(-70, -88.75);
+        glVertex2f(-90, -88.75);
+        glVertex2f(-90, -77.5);
+        glVertex2f(-70, -77.5);
+        glEnd();
+        glBegin(GL_QUADS);
+        glVertex2f(290, -88.75);
+        glVertex2f(270, -88.75);
+        glVertex2f(270, -77.5);
+        glVertex2f(290, -77.5);
+        glEnd();
+        /***** FIM CANHOES *****/
+        /*****   CIDADES   *****/
+        glColor3f(0, 1, 0);
+        for (int cd = -60; cd <= 240; cd += 50)
+        {
+            if (cd != 90)
+            {
+                glBegin(GL_QUADS);
+                glVertex2f(cd + 20, -94.375); //(-75,-90),(-50,-90),(-25,-90)
+                glVertex2f(cd, -94.375);
+                glVertex2f(cd, -88.75);
+                glVertex2f(cd + 20, -88.75);
+                glEnd();
+            }
+        }
+        /***** FIM CIDADES *****/
+        /*****    BALAS    *****/
+        glColor3f(0, 0, 1);
+        for (int bl = 0; bl < explosoes.bala[2]; bl++)
         {
             glBegin(GL_QUADS);
-            glVertex2f(cd + 20, -94.375); //(-75,-90),(-50,-90),(-25,-90)
-            glVertex2f(cd, -94.375);
-            glVertex2f(cd, -88.75);
-            glVertex2f(cd + 20, -88.75);
+            glVertex2f(86 + 3 * bl, -96.625);
+            glVertex2f(84 + 3 * bl, -96.625);
+            glVertex2f(84 + 3 * bl, -92.125);
+            glVertex2f(86 + 3 * bl, -92.125);
             glEnd();
         }
-    }
-    /***** FIM CIDADES *****/
-    /*****    BALAS    *****/
-    glColor3f(0, 0, 1);
-    for (int bl = 0; bl < explosoes.bala[2]; bl++)
-    {
-        glBegin(GL_QUADS);
-        glVertex2f(86 + 3 * bl, -96.625);
-        glVertex2f(84 + 3 * bl, -96.625);
-        glVertex2f(84 + 3 * bl, -92.125);
-        glVertex2f(86 + 3 * bl, -92.125);
-        glEnd();
-    }
-    for (int bl = 0; bl < explosoes.bala[0]; bl++)
-    {
-        glBegin(GL_QUADS);
-        glVertex2f(-94 + 3 * bl, -96.625);
-        glVertex2f(-96 + 3 * bl, -96.625);
-        glVertex2f(-96 + 3 * bl, -92.125);
-        glVertex2f(-94 + 3 * bl, -92.125);
-        glEnd();
-    }
-    for (int bl = 0; bl < explosoes.bala[1]; bl++)
-    {
-        glBegin(GL_QUADS);
-        glVertex2f(264 + 3 * bl, -96.625);
-        glVertex2f(262 + 3 * bl, -96.625);
-        glVertex2f(262 + 3 * bl, -92.125);
-        glVertex2f(264 + 3 * bl, -92.125);
-        glEnd();
-    }
-    drawAim();
+        for (int bl = 0; bl < explosoes.bala[0]; bl++)
+        {
+            glBegin(GL_QUADS);
+            glVertex2f(-94 + 3 * bl, -96.625);
+            glVertex2f(-96 + 3 * bl, -96.625);
+            glVertex2f(-96 + 3 * bl, -92.125);
+            glVertex2f(-94 + 3 * bl, -92.125);
+            glEnd();
+        }
+        for (int bl = 0; bl < explosoes.bala[1]; bl++)
+        {
+            glBegin(GL_QUADS);
+            glVertex2f(264 + 3 * bl, -96.625);
+            glVertex2f(262 + 3 * bl, -96.625);
+            glVertex2f(262 + 3 * bl, -92.125);
+            glVertex2f(264 + 3 * bl, -92.125);
+            glEnd();
+        }
+        drawAim();
     }
     glutSwapBuffers();
 }
 
 void keyboardPress(unsigned char key, int x, int y)
 {
-    switch (key)
+    if(!emPlacar)
     {
-    case 27:
-        exit(0);
-        break;
-    case 13:
-        if(!comecou&&!opcao)
-            comecou=true;
-        break;
-    case 'f':
-        if (fullscreen)
+        switch (key)
         {
-            glutSetCursor(GLUT_CURSOR_FULL_CROSSHAIR);
-            glutReshapeWindow(800, 450);
-            glutPositionWindow(50, 50);
+        case 27:
+            exit(0);
+            break;
+        case 13:
+            if(!comecou&&!opcao)
+                comecou=true;
+            else if(!comecou && opcao)
+                emPlacar = true;
+            break;
+        case 'f':
+            if (fullscreen)
+            {
+                glutSetCursor(GLUT_CURSOR_FULL_CROSSHAIR);
+                glutReshapeWindow(800, 450);
+                glutPositionWindow(50, 50);
+            }
+            else
+            {
+                glutSetCursor(GLUT_CURSOR_NONE);
+                glutFullScreen();
+            }
+            fullscreen = !fullscreen;
+            break;
         }
-        else
-        {
-            glutSetCursor(GLUT_CURSOR_NONE);
-            glutFullScreen();
-        }
-        fullscreen = !fullscreen;
-        break;
     }
+    else
+    {
+        if(!confirmaInsercao)
+        {
+            if(key== 13)
+            {
+                string str(nome);
+                confirmaInsercao = true;
+                pl->inserirJogador(nome, 10);
+                pl->salvarPontuacao();
+
+            }
+            else if(key == 8)
+                nome[indPlacar] = ' ';
+        }
+    }
+
+
+
 }
 
 void init()
@@ -215,46 +325,47 @@ void init()
 }
 void mouse(int button, int state, int x, int y)
 {
-    if(comecou){
-    if (button == GLUT_LEFT_BUTTON)
+    if(comecou)
     {
-        if (state == GLUT_DOWN)
+        if (button == GLUT_LEFT_BUTTON)
         {
-            mouseDown = true;
-            float xreal = (float)(mousex / 2 - 100);
-            float yreal = (float)(-1 * mousey / 2 + 125);
-            if (xreal < 20 && explosoes.bala[0] > 0)
+            if (state == GLUT_DOWN)
             {
-                explosoes.addObjeto(xreal, yreal, -80, -83.125);
-                explosoes.bala[0]--;
-            }
-            else if (xreal > 180 && explosoes.bala[1] > 0)
-            {
-                explosoes.addObjeto(xreal, yreal, 280, -83.125);
-                explosoes.bala[1]--;
-            }
-            else if (explosoes.bala[2] > 0)
-            {
-                explosoes.addObjeto(xreal, yreal, 100, -83.125);
-                explosoes.bala[2]--;
-            }
-            else if (xreal < 100 && explosoes.bala[0] > 0)
-            {
-                explosoes.addObjeto(xreal, yreal, -80, -83.125);
-                explosoes.bala[0]--;
-            }
-            else if (explosoes.bala[1] > 0)
-            {
-                explosoes.addObjeto(xreal, yreal, 280, -83.125);
-                explosoes.bala[1]--;
-            }
-            else if (explosoes.bala[0] > 0)
-            {
-                explosoes.addObjeto(xreal, yreal, -80, -83.125);
-                explosoes.bala[0]--;
+                mouseDown = true;
+                float xreal = (float)(mousex / 2 - 100);
+                float yreal = (float)(-1 * mousey / 2 + 125);
+                if (xreal < 20 && explosoes.bala[0] > 0)
+                {
+                    explosoes.addObjeto(xreal, yreal, -80, -83.125);
+                    explosoes.bala[0]--;
+                }
+                else if (xreal > 180 && explosoes.bala[1] > 0)
+                {
+                    explosoes.addObjeto(xreal, yreal, 280, -83.125);
+                    explosoes.bala[1]--;
+                }
+                else if (explosoes.bala[2] > 0)
+                {
+                    explosoes.addObjeto(xreal, yreal, 100, -83.125);
+                    explosoes.bala[2]--;
+                }
+                else if (xreal < 100 && explosoes.bala[0] > 0)
+                {
+                    explosoes.addObjeto(xreal, yreal, -80, -83.125);
+                    explosoes.bala[0]--;
+                }
+                else if (explosoes.bala[1] > 0)
+                {
+                    explosoes.addObjeto(xreal, yreal, 280, -83.125);
+                    explosoes.bala[1]--;
+                }
+                else if (explosoes.bala[0] > 0)
+                {
+                    explosoes.addObjeto(xreal, yreal, -80, -83.125);
+                    explosoes.bala[0]--;
+                }
             }
         }
-    }
     }
 }
 void motion(int x, int y) //funcao que pega os valores do mouse em tempo real
@@ -265,20 +376,58 @@ void motion(int x, int y) //funcao que pega os valores do mouse em tempo real
 
 void specialKeysPress(int key, int x, int y)
 {
-	switch(key)
-	{
-		case GLUT_KEY_UP:
-		    opcao=0;
+    if(!emPlacar)
+    {
+        switch(key)
+        {
+        case GLUT_KEY_UP:
+            opcao=0;
             break;
-		case GLUT_KEY_DOWN:
-		    opcao=1;
+        case GLUT_KEY_DOWN:
+            opcao=1;
             break;
-		case GLUT_KEY_RIGHT:
+        case GLUT_KEY_RIGHT:
             break;
         case GLUT_KEY_LEFT:
             break;
-	}
-	glutPostRedisplay();
+        }
+    }
+    else
+    {
+        if(!confirmaInsercao)
+        {
+            switch(key)
+            {
+            case GLUT_KEY_UP:
+
+                nome[indPlacar]++;
+                if(nome[indPlacar]>90)
+                    nome[indPlacar] = 65;
+                break;
+            case GLUT_KEY_DOWN:
+                nome[indPlacar]--;
+                if(nome[indPlacar]<65)
+                    nome[indPlacar] = 90;
+                break;
+            case GLUT_KEY_RIGHT:
+
+                if(indPlacar<=18)
+                    indPlacar++;
+                if(nome[indPlacar] == ' ')
+                    nome[indPlacar] = 65;
+                break;
+            case GLUT_KEY_LEFT:
+
+                if(indPlacar>=1)
+                    indPlacar--;
+                if(nome[indPlacar] == ' ')
+                    nome[indPlacar] = 65;
+                break;
+            }
+        }
+    }
+
+    glutPostRedisplay();
 }
 void idle()
 {
@@ -293,64 +442,67 @@ void idle()
     explosoes.atualizaTempo(dt);
     inimigos.atualizaTempo(dt);
     //funcoes do professor para variacao de tempo a cima
-    if(!comecou){
-
-    }else{
-    jogadorx = (float)(mousex / 2 - 100);
-    jogadory = (float)(-1 * mousey / 2 + 125);
-    /***** MISSEIS *****/
-    if (clok > 10)
+    if(!comecou)
     {
-        for (int i = 0; i < 2; i++)
-        {
-            std::mt19937 rng(rand());
-            std::uniform_int_distribution<int> uni(-100, 300);
-            auto random_x = uni(rng);
-            std::uniform_int_distribution<int> duni(0, 8);
-            auto cit = duni(rng);
-            if (cit == 1)
-            {
-                inimigos.addObjeto(100, -77.5, random_x, 300, 50000);
-            }
-            else if (cit == 0)
-            {
-                inimigos.addObjeto(-80, -77.5, random_x, 300, 50000);
-            }
-            else if (cit == 2)
-            {
-                inimigos.addObjeto(280, -77.5, random_x, 300, 50000);
-            }
-            else if (cit == 3)
-            {
-                inimigos.addObjeto(-50, -88.75, random_x, 300, 50000);
-            }
-            else if (cit == 4)
-            {
-                inimigos.addObjeto(0, -88.75, random_x, 300, 50000);
-            }
-            else if (cit == 5)
-            {
-                inimigos.addObjeto(50, -88.75, random_x, 300, 50000);
-            }
-            else if (cit == 6)
-            {
-                inimigos.addObjeto(150, -88.75, random_x, 300, 50000);
-            }
-            else if (cit == 7)
-            {
-                inimigos.addObjeto(200, -88.75, random_x, 300, 50000);
-            }
-            else if (cit == 8)
-            {
-                inimigos.addObjeto(250, -88.75, random_x, 300, 50000);
-            }
-        }
-        clok = 0;
+
     }
-    inimigos.colisao(explosoes);
-    inimigos.colisao(inimigos);
-    inimigos.dividir();
-    clok += dt;
+    else if(comecou)
+    {
+        jogadorx = (float)(mousex / 2 - 100);
+        jogadory = (float)(-1 * mousey / 2 + 125);
+        /***** MISSEIS *****/
+        if (clok > 10)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                std::mt19937 rng(rand());
+                std::uniform_int_distribution<int> uni(-100, 300);
+                auto random_x = uni(rng);
+                std::uniform_int_distribution<int> duni(0, 8);
+                auto cit = duni(rng);
+                if (cit == 1)
+                {
+                    inimigos.addObjeto(100, -77.5, random_x, 300, 50000);
+                }
+                else if (cit == 0)
+                {
+                    inimigos.addObjeto(-80, -77.5, random_x, 300, 50000);
+                }
+                else if (cit == 2)
+                {
+                    inimigos.addObjeto(280, -77.5, random_x, 300, 50000);
+                }
+                else if (cit == 3)
+                {
+                    inimigos.addObjeto(-50, -88.75, random_x, 300, 50000);
+                }
+                else if (cit == 4)
+                {
+                    inimigos.addObjeto(0, -88.75, random_x, 300, 50000);
+                }
+                else if (cit == 5)
+                {
+                    inimigos.addObjeto(50, -88.75, random_x, 300, 50000);
+                }
+                else if (cit == 6)
+                {
+                    inimigos.addObjeto(150, -88.75, random_x, 300, 50000);
+                }
+                else if (cit == 7)
+                {
+                    inimigos.addObjeto(200, -88.75, random_x, 300, 50000);
+                }
+                else if (cit == 8)
+                {
+                    inimigos.addObjeto(250, -88.75, random_x, 300, 50000);
+                }
+            }
+            clok = 0;
+        }
+        inimigos.colisao(explosoes);
+        inimigos.colisao(inimigos);
+        inimigos.dividir();
+        clok += dt;
     }
     tLast = t; //atualiza o tempo, deixar no fim da idle
 }
